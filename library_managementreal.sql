cd4637fcfb98a9ac689d3ee51dba2f06179e97b9
yyuujjiinn <manubayeugene35@gmail.com>
2026-03-25 21:02:04 +0800

    Add files via upload

---
+++ library_managementreal.sql
@@ -0,0 +1,452 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 25, 2026 at 02:01 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors`
+--
+
+CREATE TABLE `authors` (
+  `AuthorID` int(50) NOT NULL,
+  `AuthorName` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `authors`
+--
+
+INSERT INTO `authors` (`AuthorID`, `AuthorName`) VALUES
+(1, 'Rizal');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `BookID` int(50) NOT NULL,
+  `BookTitle` varchar(100) NOT NULL,
+  `CategoryID` int(50) NOT NULL,
+  `PublisherId` int(50) NOT NULL,
+  `Quantity` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`BookID`, `BookTitle`, `CategoryID`, `PublisherId`, `Quantity`) VALUES
+(2, 'Noli', 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_archive`
+--
+
+CREATE TABLE `book_archive` (
+  `ArchiveID` int(11) NOT NULL,
+  `BookID` int(50) NOT NULL,
+  `BookTitle` varchar(100) NOT NULL,
+  `CategoryID` int(50) NOT NULL,
+  `PublisherId` int(50) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `DeletedAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_authors`
+--
+
+CREATE TABLE `book_authors` (
+  `BookID` int(50) NOT NULL,
+  `AuthorID` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book_copies`
+--
+
+CREATE TABLE `book_copies` (
+  `Status` varchar(100) NOT NULL,
+  `BookID` int(11) NOT NULL,
+  `AuthorID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrow`
+--
+
+CREATE TABLE `borrow` (
+  `BorrowID` int(50) NOT NULL,
+  `UserID` int(50) NOT NULL,
+  `BookID` int(50) NOT NULL,
+  `BorrowDate` varchar(15) NOT NULL,
+  `DueDate` varchar(15) NOT NULL,
+  `Returndate` varchar(15) NOT NULL,
+  `Status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `CategoryID` int(50) NOT NULL,
+  `CategoryName` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`CategoryID`, `CategoryName`) VALUES
+(1, 'History');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `CourseID` int(50) NOT NULL,
+  `CourseName` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fine`
+--
+
+CREATE TABLE `fine` (
+  `FineID` int(50) NOT NULL,
+  `BorrowID` int(50) NOT NULL,
+  `Amount` int(20) NOT NULL,
+  `PaidStatus` varchar(10) NOT NULL,
+  `DateIssued` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `materials`
+--
+
+CREATE TABLE `materials` (
+  `MaterialID` int(11) NOT NULL,
+  `MaterialName` varchar(255) NOT NULL,
+  `Quantity` int(11) DEFAULT 0,
+  `CategoryName` varchar(100) NOT NULL,
+  `Category` varchar(100) DEFAULT 'General'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `materials`
+--
+
+INSERT INTO `materials` (`MaterialID`, `MaterialName`, `Quantity`, `CategoryName`, `Category`) VALUES
+(1, 'Journal of IT', 5, 'Journal', 'General'),
+(2, 'AI Research 2024', 2, 'Research', 'General'),
+(3, 'Thesis on Robotics', 1, 'Research', 'General'),
+(4, 'National Geographic', 10, 'Magazine', 'General'),
+(5, 'PC World', 8, 'Magazine', 'General'),
+(6, 'Journal of IT Research', 5, 'Journal', 'Journal'),
+(7, 'Study on AI Ethics', 2, '', 'Research'),
+(8, 'Gadget Magazine', 10, 'Magazine', 'Magazine'),
+(9, 'Manila Times', 15, '', 'Newspaper');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `ID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `Message` text NOT NULL,
+  `Status` varchar(50) DEFAULT 'Unread',
+  `CreatedAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`ID`, `UserID`, `Message`, `Status`, `CreatedAt`) VALUES
+(1, 9, 'Hello', 'Read', '2026-03-25 10:52:00'),
+(2, 9, 'he', 'Read', '2026-03-25 10:56:13'),
+(3, 9, 'tt', 'Read', '2026-03-25 11:03:44'),
+(4, 9, '123', 'Read', '2026-03-25 11:07:43'),
+(5, 9, 'Burat', 'Read', '2026-03-25 12:12:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publisher`
+--
+
+CREATE TABLE `publisher` (
+  `PublisherID` int(50) NOT NULL,
+  `PublisherName` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `publisher`
+--
+
+INSERT INTO `publisher` (`PublisherID`, `PublisherName`) VALUES
+(1, 'Liwayway');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `RoleID` int(50) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(20) NOT NULL,
+  `CourseID` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`RoleID`, `Name`, `Email`, `Password`, `CourseID`) VALUES
+(9, 'Yujin', 'jinyu3097@gmail.com', '123', 123),
+(10, 'Pasion', 'manubayeugene35@gmail.com', '123', 123),
+(11, 'Russel', 'burat@gmail.com', '123', 0),
+(12, 'Ash', 'tite@gmail.com', '123', 0),
+(13, 'Mica', 'tite1@gmail.com', '123', 123);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `RoleID` int(11) NOT NULL,
+  `StudentID` int(11) NOT NULL,
+  `AdminID` int(11) NOT NULL,
+  `VisitorID` int(11) NOT NULL,
+  `StaffID` int(11) NOT NULL,
+  `FacultyID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+INSERT INTO `user_roles` (`RoleID`, `StudentID`, `AdminID`, `VisitorID`, `StaffID`, `FacultyID`) VALUES
+(9, 9, 0, 0, 0, 0),
+(10, 0, 10, 0, 0, 0),
+(11, 0, 0, 11, 0, 0),
+(12, 0, 0, 0, 12, 0),
+(13, 0, 0, 0, 0, 13);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`AuthorID`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`BookID`),
+  ADD KEY `CategoryID` (`CategoryID`),
+  ADD KEY `PublisherId` (`PublisherId`);
+
+--
+-- Indexes for table `book_archive`
+--
+ALTER TABLE `book_archive`
+  ADD PRIMARY KEY (`ArchiveID`);
+
+--
+-- Indexes for table `book_authors`
+--
+ALTER TABLE `book_authors`
+  ADD PRIMARY KEY (`BookID`,`AuthorID`),
+  ADD KEY `AuthorID` (`AuthorID`);
+
+--
+-- Indexes for table `book_copies`
+--
+ALTER TABLE `book_copies`
+  ADD PRIMARY KEY (`Status`);
+
+--
+-- Indexes for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD PRIMARY KEY (`BorrowID`),
+  ADD KEY `StudentID` (`UserID`),
+  ADD KEY `BookID` (`BookID`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`CategoryID`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`CourseID`);
+
+--
+-- Indexes for table `fine`
+--
+ALTER TABLE `fine`
+  ADD PRIMARY KEY (`FineID`),
+  ADD KEY `BorrowID` (`BorrowID`);
+
+--
+-- Indexes for table `materials`
+--
+ALTER TABLE `materials`
+  ADD PRIMARY KEY (`MaterialID`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `publisher`
+--
+ALTER TABLE `publisher`
+  ADD PRIMARY KEY (`PublisherID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`RoleID`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `CourseID` (`CourseID`);
+
+--
+-- Indexes for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`RoleID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `AuthorID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `BookID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `book_archive`
+--
+ALTER TABLE `book_archive`
+  MODIFY `ArchiveID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `CategoryID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `materials`
+--
+ALTER TABLE `materials`
+  MODIFY `MaterialID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `publisher`
+--
+ALTER TABLE `publisher`
+  MODIFY `PublisherID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `RoleID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  MODIFY `RoleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book_authors`
+--
+ALTER TABLE `book_authors`
+  ADD CONSTRAINT `book_authors_ibfk_1` FOREIGN KEY (`BookID`) REFERENCES `books` (`BookID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `book_authors_ibfk_2` FOREIGN KEY (`AuthorID`) REFERENCES `authors` (`AuthorID`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
