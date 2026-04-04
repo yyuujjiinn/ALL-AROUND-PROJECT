b066f21ce6e9c14de9257390051a966e97e101b7
russkie <dionisiojanrussel@gmail.com>
2026-04-04 23:27:29 +0800

    Delete RusselCopies/RusselCopies/DeBelen_copies directory

--- RusselCopies/RusselCopies/DeBelen_copies/mark_read.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'connect.php';
-if (isset($_POST['id'])) {
-    $id = $_POST['id'];
-    // Update the Status to 'Read' for this specific notification
-    $conn->query("UPDATE notifications SET Status = 'Read' WHERE ID = '$id'");
-}
-?>
